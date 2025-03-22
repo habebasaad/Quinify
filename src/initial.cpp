d19ab26f70146a9e3011bf0a6646fba3e94b2d57
Boody624 <abdelrahman_osama-@aucegypt.edu>
2025-03-22 14:00:47 +0200

    OOP approach

---
+++ src/initial.cpp
@@ -0,0 +1,15 @@
+#include "Expression.h"
+#include "Table.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Expression expr("input.txt");
+    expr.printTerms();
+    
+    Table table(expr.minterms, expr.dontCares);
+    table.printPrimeImplicants();
+    
+    return 0;
+}
